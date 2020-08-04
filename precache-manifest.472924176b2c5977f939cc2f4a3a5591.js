32eca1226e7be07e1b065801a38d27ca69acc5f7
Singh <008070@usremwfh605729.lan>
2020-08-03 21:51:31 -0700

    Updates

---
+++ precache-manifest.472924176b2c5977f939cc2f4a3a5591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39157487784d41c4c63b58b941aa0b5c",
+    "url": "/amarjitsingh65/react-proj-basic/index.html"
+  },
+  {
+    "revision": "65c909db3194a4cbe4f4",
+    "url": "/amarjitsingh65/react-proj-basic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b91e1254827e7d2e6f5",
+    "url": "/amarjitsingh65/react-proj-basic/static/js/2.c7adede6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amarjitsingh65/react-proj-basic/static/js/2.c7adede6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c909db3194a4cbe4f4",
+    "url": "/amarjitsingh65/react-proj-basic/static/js/main.49d51086.chunk.js"
+  },
+  {
+    "revision": "cf8198483506a832a5fc",
+    "url": "/amarjitsingh65/react-proj-basic/static/js/runtime-main.980b7995.js"
+  }
+]);
